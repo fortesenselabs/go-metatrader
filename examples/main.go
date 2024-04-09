303cb2abc03c5ae89af51ff2e8d2ea509ac9f53f
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-04-09 11:17:17 +0100

    update examples

--- examples/main.go
+++ examples/main.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 
 	"github.com/FortesenseLabs/go-metatrader/metatrader"
-	"github.com/FortesenseLabs/go-metatrader/metatrader/actiontype"
-	"github.com/FortesenseLabs/go-metatrader/metatrader/timeframes"
 )
 
 func main() {
@@ -27,23 +25,85 @@ func main() {
 	// Print the response
 	fmt.Println(accountInfo)
 
-	// get balance
-	balance, err := mt.GetBalance()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// // get balance
+	// balance, err := mt.GetBalance()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
 
-	// Print the response
-	fmt.Println(balance)
+	// // Print the response
+	// fmt.Println(balance)
 
-	// get historical data
-	history, err := mt.GetHistoricalData("Step Index", timeframes.D1, actiontype.PRICE, "01-02-2024 00:00:00", "13-02-2024 00:00:00")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	// always get the current price
+	// for {
+	// 	// get current price
+	// 	currentPrice, err := mt.GetCurrentPrice("Step Index")
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		return
+	// 	}
 
-	// Print the response
-	fmt.Println(history)
+	// 	// Print the response
+	// 	fmt.Println(currentPrice)
+
+	// 	time.Sleep(1 * time.Millisecond)
+	// }
+
+	// // get historical data
+	// history, err := mt.GetHistoricalData("Step Index", timeframes.D1, actiontype.PRICE, "13-02-2021 00:00:00", "13-02-2024 00:00:00")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// // Print the response
+	// fmt.Println(history)
+
+	// get orders
+	// orders, err := mt.GetOrders()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// // Print the response
+	// fmt.Println(orders)
+
+	// get positions
+	// positions, err := mt.GetPositions()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// // Print the response
+	// fmt.Println(positions)
+
+	// currentPrice, err := mt.GetCurrentPrice("Step Index")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// fmt.Println(currentPrice)
+
+	// pips := 100
+	// stopLevel := 0.1
+	// normalizedPips := float64(pips) * stopLevel
+
+	// // BUY
+	// stopLoss := float64(currentPrice.Tick.Bid) - normalizedPips
+	// takeProfit := float64(currentPrice.Tick.Bid) + normalizedPips
+
+	// fmt.Println(stopLoss, takeProfit)
+
+	// err = mt.Buy("Step Index", 0.1, stopLoss, takeProfit, 5)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// fmt.Println("BUY Order Sent")
+
 }
