1831099f57a47b9fd3c840c3d6bc9b27e222039a
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-04-09 11:16:30 +0100

    update utils

--- metatrader/utils/utils.go
+++ metatrader/utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 func ConvertDateToUTC(dateStr string, _format string) (string, error) {
 	// Parse the input date string based on the given format
@@ -18,7 +21,41 @@ func ConvertDateToUTC(dateStr string, _format string) (string, error) {
 	return utcDateStr, nil
 }
 
+// func ConvertDataFrameSeries(dfSereis series.Series) []string {
+// 	// Create an array of strings
+// 	var data []string
+
+// 	// Convert the dataframe series to a string array
+// 	for _, value := range dfSereis.Values {
+// 		data = append(data, value.(string))
+// 	}
+
+// 	return data
+// }
+
+// Contains checks if a string is present in a slice
 func Contains(slice []string, item string) bool {
-	// Check if slice contains item
+	for _, value := range slice {
+		if value == item {
+			return true
+		}
+	}
 	return false
+}
+
+// Helper function to parse float64 from string
+func ParseFloat(s string) float64 {
+	f, _ := strconv.ParseFloat(s, 64)
+	return f
+}
+
+// Helper function to parse int from string
+func ParseInt(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+func ParseInt64(s string) int64 {
+	i, _ := strconv.Atoi(s)
+	return int64(i)
 }
