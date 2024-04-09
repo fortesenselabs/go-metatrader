03e6296713cfbfde4bce2b35d648fc91c593112b
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-04-09 11:16:43 +0100

    update action types

--- metatrader/actiontype/actiontype.go
+++ metatrader/actiontype/actiontype.go
@@ -4,3 +4,12 @@ const (
 	PRICE  = "PRICE"
 	TRADES = "TRADES" // Trades - only requires the actionType parameter
 )
+
+var ACTIONS = []string{
+	"ORDER_TYPE_BUY",
+	"ORDER_TYPE_SELL",
+	"ORDER_TYPE_BUY_LIMIT",
+	"ORDER_TYPE_SELL_LIMIT",
+	"ORDER_TYPE_BUY_STOP",
+	"ORDER_TYPE_SELL_STOP",
+}
